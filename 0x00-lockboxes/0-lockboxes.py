6d871f3fc6bd06e7f97a7342a70e8d4e6dda8e6f
KoeusIss <sebri.issam@gmail.com>
2020-12-10 17:37:03 +0100

    commit the lockboxe task

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Lockbox module"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened or not
+
+    Args:
+        boxes (list[list]): boxes is the list of box that contains keys
+
+    Returns:
+        (bool): True if we can open all the boxes, otherwise return False
+
+    """
+    opens = [True] + [False] * (len(boxes) - 1)
+
+    def open_box(box):
+        """Open all the associate box"""
+
+        if len(box) == 0 or all([opens[k] for k in box]):
+            return
+        else:
+            for key in box:
+                opens[key] = True
+                open_box(boxes[key])
+        open_box(boxes[0])
+        return all(opens)
