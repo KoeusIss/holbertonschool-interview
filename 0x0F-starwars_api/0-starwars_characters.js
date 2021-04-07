1350ebe38f7b5c7ba6b8e5599537de8483b8afef
KoeusIss <sebri.issam@gmail.com>
2021-04-07 21:26:13 +0100

    update the starwars

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-
-const film = process.argv[2];
-const baseUrl = `https://swapi-api.hbtn.io/api/films/${film}`;
-
-request(baseUrl, async (error, response, body) => {
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+request(url, async (error, response, body) => {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    for (const character in characters) {
+    for (const charId in characters) {
       await new Promise((resolve, reject) => {
-        request(characters[character], (error, response, body) => {
+        request(characters[charId], (error, response, body) => {
           if (!error) {
             console.log(JSON.parse(body).name);
             resolve();
